39e48dcaf2bdd5b34ff0a7b5a408dad27206e251
zhornyk-maria <zhornyk.maria@gmail.com>
2024-03-25 13:10:39 +0200

    add validation for form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from 'yup';
 import css from './ContactForm.module.css';
 
 const initialValues = {
@@ -6,16 +7,26 @@ const initialValues = {
   number: ""
 }
 
+const MIN_CHAR_VALIDATION = 3;
+const MAX_CHAR_VALIDATION = 50;
+
+const contactFormSchema = yup.object({
+  name: yup.string().min(MIN_CHAR_VALIDATION, "Too Short!").max(MAX_CHAR_VALIDATION).required("Required"),
+  number: yup.string().min(MIN_CHAR_VALIDATION, "Too Short!").max(MAX_CHAR_VALIDATION).required("Required").matches(/^[0-9]+$/, "Phone number must contain only digits"),
+});
+
 const ContactForm = ({handleSubmit}) => {
   
   return (
 
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik initialValues={initialValues} validationSchema={contactFormSchema} onSubmit={handleSubmit}>
         <Form className={css.form}>
           <label htmlFor="name">Name</label>
-          <Field type="text" id="name" name="name" className={css.input} required />
+          <Field type="text" id="name" name="name" className={css.input} />
+          <ErrorMessage component="p" name="name"/>
           <label htmlFor="number">Number</label>
-          <Field type="tel" id="number" name="number" className={css.input} required/>
+          <Field type="tel" id="number" name="number" className={css.input} />
+          <ErrorMessage component="p" name="number"/>
           <button type="submit" className={css.button}>Add contact</button>
         </Form>
     </Formik>
